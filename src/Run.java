62ba0fcb771153adaf9f5cca8f2cd0c8c692f2aa
filippo <filippo-ceradini>
2021-11-17 00:56:26 +0100

    GUI 0.1 Login page(password not working) Admin area Gui Staff Edit Gui

--- src/Run.java
+++ src/Run.java
@@ -8,8 +8,8 @@ import java.net.URL;
 
 public class Run {
     public static void main(String[] args) throws IOException {
-
-        GUI.login();
+        StaffGUI.ManageStaff();
+        //GUI.login();
     }
 
 }
